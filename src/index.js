992c7ca292eb7d139d4252bfec3b23d68c1ad446
Zeeshan Ali <zeeshanali93@live.com>
2020-10-10 23:40:04 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-import GrandParent from './GRAND_PARENT';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GrandParent />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
